8ce3cccde23f5bd27e139cee2cda8a378abb3dce
Chris Tian <chris.tian@biposervice.com>
2025-12-14 21:12:35 +0800

    chore: simplify utils package structure and exports
    Remove TypeScript declaration field and simplify package exports to only include cn utility. Remove project references and clean up tsconfig.json structure. This reduces the package's scope to focus on core utility functions.

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,8 +1,2 @@
 // Re-export all utilities
-export { cn } from './utils';
-export * from './date';
-export * from './format';
-export * from './validation';
-export * from './api';
-export * from './storage';
-export * from './auth';
+export { cn } from './utils'
